0c1b6da8db0e4cf6adbc7222e2afeb5dca0ae51a
Omar Mohamed <omar.mohamed197@msa.edu.eg>
2026-04-06 21:38:47 +0200

    fix cannot get root

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
 const queueRouter = require('./routes/queue');
 app.use('/api/queue', queueRouter);
 
